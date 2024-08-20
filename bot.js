47a447979dbb9198564dcaeceae77bc47ac291b8
nikita-shalamov <nikita.shalamovv@yandex.ru>
2024-08-20 23:22:07 +0500

    add chats

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
-// import dotenv from 'dotenv';
-// import TelegramBot from 'node-telegram-bot-api';
-// import config from 'config';
+import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
+import config from 'config';
 
-// dotenv.config();
+dotenv.config();
 
-// const token = config.get("TOKEN");
-// const webAppLink = config.get("LINK");
+const token = config.get("TOKEN");
+const webAppLink = config.get("LINK");
 
-// const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true });
 
-// bot.on('message', async (msg) => {
-//     const chatId = msg.chat.id;
-//     const text = msg.text;
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-//     if (text === '/start') {
-//         await bot.sendMessage(chatId, '*Привет! 👋*\n\nГотовы встретить свою судьбу? 💘\n\nНажмите на кнопку ниже, чтобы начать знакомиться с новыми людьми прямо сейчас!', {
-//             parse_mode: 'Markdown',
-//             reply_markup: {
-//                 inline_keyboard: [
-//                     [{ text: 'Открыть приложение', web_app: { url: webAppLink } }]
-//                 ],
-//                 remove_keyboard: true
-//             }
-//         });
-//     }
-// });
+    if (text === '/start') {
+        await bot.sendMessage(chatId, '*Привет! 👋*\n\nГотовы встретить свою судьбу? 💘\n\nНажмите на кнопку ниже, чтобы начать знакомиться с новыми людьми прямо сейчас!', {
+            parse_mode: 'Markdown',
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Открыть приложение', web_app: { url: webAppLink } }]
+                ],
+                remove_keyboard: true
+            }
+        });
+    }
+});
